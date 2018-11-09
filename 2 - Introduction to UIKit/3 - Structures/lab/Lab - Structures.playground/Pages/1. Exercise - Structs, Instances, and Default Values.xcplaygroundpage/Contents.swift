ee3e900bf4e86b4ee5c33f1252819ae53a8e7668
Christine Tham <chris.tham@hellotham.com>
2018-11-10 10:18:59 +1100

    Initial commit

---
+++ 2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,50 @@
+/*:
+ ## Exercise - Structs, Instances, and Default Values
+ 
+ Imagine you are creating some kind of app for monitoring location. Create a `GPS` struct with two variable properties, latitude and longitude, both with default values of 0.0.
+ */
+struct GPS {
+    var latitude = 0.0
+    var longitude = 0.0
+}
+
+/*:
+ Create a variable instance of `GPS` called `somePlace`. It should be initialized without supplying any arguments. Print out the latitude and longitude of `somePlace`, which should be 0.0 for both.
+ */
+var somePlace = GPS()
+print(somePlace.latitude, somePlace.longitude)
+/*:
+ Change `somePlace`'s latitude to 51.514004, and the longitude to 0.125226, then print the updated values.
+ */
+somePlace.latitude = 51.514004
+somePlace.longitude = 0.125226
+print(somePlace.latitude, somePlace.longitude)
+/*:
+ Now imagine you are making a social app for sharing your favorite books. Create a `Book` struct with four variable properties: `title`, `author`, `pages`, and `price`. The default values for both `title` and `author` should be an empty string. `pages` should default to 0, and `price` should default to 0.0.
+ */
+struct Book {
+    var title = ""
+    var author = ""
+    var pages = 0
+    var price = 0.0
+}
+
+/*:
+ Create a variable instance of `Book` called `favoriteBook` without supplying any arguments. Print out the title of `favoriteBook`. Does it currently reflect the title of your favorite book? Probably not. Change all four properties of `favoriteBook` to reflect your favorite book. Then using the properties of `favoriteBook`, print out facts about the book.
+ */
+var favouriteBook = Book()
+print(favouriteBook.title)
+favouriteBook.title = "Lord of the Rings"
+favouriteBook.author = "J. R. R. Tolkien"
+favouriteBook.pages = 1000
+favouriteBook.price = 19.99
+print(favouriteBook.title, favouriteBook.author, favouriteBook.pages, favouriteBook.price)
+/*:
+ Make a `Height` struct with two variable properties, `heightInInches` and `heightInCentimeters`. Both should be of type `Double`.
+ 
+ Create two custom initializers. One initializer will take a `Double` argument that represents height in inches. The other initializer will take a `Double` argument that represents height in centimeters. Each initializer should take the passed in value and use it to set the property that corresponds to the unit of measurement passed in. It should then set the other property by calculating the right value from the passed in value. Hint: *1 inch = 2.54 centimeters*.
+ 
+ - Example: If you use the initializer for inches to pass in a height of 65, the initializer should set `heightInInches` to 65 and `heightInCentimeters` to 165.1.
+ */
+
+//: page 1 of 10  |  [Next: App Exercise - Workout Tracking](@next)
